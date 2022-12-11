066551656a4ed3a04f462456f0ccfb6a538db7f3
Alwinder Singh <100020772+AlwinderSingh@users.noreply.github.com>
2022-12-11 19:06:10 +0530

    Delete db.js

--- db.js
+++
@@ -1,7 +0,0 @@
-const { Pool, Client } = require('pg')
-const conn="postgres://database_yogaflex_user:88IZWitXxFzISI1evKkaT6CDMYm3Zwlz@dpg-ceasm0da4996mecfcls0-a.oregon-postgres.render.com/database_yogaflex"
-const conn2="postgres://database_yogaflex_user:88IZWitXxFzISI1evKkaT6CDMYm3Zwlz@dpg-ceasm0da4996mecfcls0-a/database_yogaflex"
-const db = new Client({
-    conn2,
-    })
-module.exports= db
