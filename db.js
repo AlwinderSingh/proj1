5085ed34e01b5f58cecc3f2d3824515f8555441d
Alwinder Singh <100020772+AlwinderSingh@users.noreply.github.com>
2022-12-11 18:30:28 +0530

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database("./Members.db",(err)=>{
-    if(err){
-        console.log(err)
-    }
-    console.log("Connected to database")
-})
-module.exports= db
